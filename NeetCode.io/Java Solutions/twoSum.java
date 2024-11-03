85d4dbce016d2f7d484d0e393e2cb2cdcc719dc1
ImLegless <86304879+ImLegless@users.noreply.github.com>
2024-11-02 22:58:53 -0500

    java solutions

--- NeetCode.io/Java Solutions/twoSum.java
+++ NeetCode.io/Java Solutions/twoSum.java
@@ -1,16 +1,60 @@
 import java.util.HashMap;
-import java.util.Map;
+
+/*
+
+Given an array of integers nums and an integer target,
+ return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+You may assume that every input has exactly one pair of
+ indices i and j that satisfy the condition.
+Return the answer with the smaller index first.
+
+Example 1:
+
+Input:
+nums = [3,4,5,6], target = 7
+Output: [0,1]
+
+
+Constraints:
+2 <= nums.length <= 1000
+-10,000,000 <= nums[i] <= 10,000,000
+-10,000,000 <= target <= 10,000,000
+
+ */
+
 
 public class twoSum {
-    public int[] twoSum(int[] numbers, int target) {
-        Map<Integer, Integer> mp = new HashMap<>();
-        for (int i = 0; i < numbers.length; i++) {
-            int tmp = target - numbers[i];
-            if (mp.containsKey(tmp)) {
-                return new int[] { mp.get(tmp), i + 1 };
+
+
+    public int[] twoSum(int[] nums, int target) {
+
+        //Create a new hashmap
+        HashMap<Integer, Integer> prevMap = new HashMap<>();
+
+        //Loop through the length of given array
+        for (int i = 0; i < nums.length; i++) {
+            //Declare and initialize @int num with value given at index i of @array nums
+            int num = nums[i];
+            //Declare and initialize @int diff with the given @int target - @int num (current value at index)
+            int diff = target - num;
+
+
+            //if the hashmap contains the value given by
+            //the difference of the target value - current value at index
+            //
+            //return a new array of type @int with the value of @HashMap prevMap with the given
+            //key @int diff
+            if (prevMap.containsKey(diff)) {
+                return new int[] { prevMap.get(diff), i };
             }
-            mp.put(numbers[i], i + 1);
+
+            //associate the value @int i with the key @int diff
+            prevMap.put(num, i);
         }
-        return new int[0];
+
+        //return empty int[]
+        return new int[] {};
     }
 }
+
