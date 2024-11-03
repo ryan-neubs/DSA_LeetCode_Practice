bbfdfe22ee1417a8ad2ba2a0eb005fbb243bcccc
ImLegless <86304879+ImLegless@users.noreply.github.com>
2024-11-02 22:53:50 -0500

    java solutions

--- NeetCode.io/Java Solutions/isPalindrome.java
+++ NeetCode.io/Java Solutions/isPalindrome.java
@@ -1,36 +1,14 @@
 
-
-
-public class test {
-
-     public static void main(String[] args){
-
-        String s = "Was it a car or a cat I saw?";
-
-        s = s.replaceAll("\\p{Space}", "taco");
-
-        System.out.println(s);
-
-
-
-
-     }
-
+class Solution {
     public boolean isPalindrome(String s) {
-
         s = s.toLowerCase();
         s = s.replaceAll("\\p{Punct}","");
-
-
+        s = s.replaceAll("\\p{Space}", "");
         String newString = new StringBuilder(s).reverse().toString();
-
         if (newString.equalsIgnoreCase(s)){
             return true;
         }
-
         return false;
     }
 }
-
-
 
