215bbaaf149299429fdf0179ba57fc34dedb34a7
Ryan Neubauer <rjneub09@gmail.com>
2024-11-12 11:58:07 -0600

    Added second solution to isPalindrome reducing space complexity

--- NeetCode.io/Python Solutions/Two_pointers/isPalindrome.py
+++ NeetCode.io/Python Solutions/Two_pointers/isPalindrome.py
@@ -6,3 +6,24 @@ def isPalindrome(s):
             clean_s += char.lower()
 
     return clean_s == clean_s[::-1]
+
+
+# This following solution reduces space complexity to O(1)
+def isPalindrome_pointers(s):
+    left, right = 0, len(s) - 1
+
+    while left < right:
+
+        while left < right and not s[left].isalnum():
+            left += 1
+        
+        while left< right and not s[right].isalnum():
+            right -= 1
+
+        if s[left].lower() != s[right].lower():
+            return False
+        
+        left += 1
+        right -= 1
+
+        return True
